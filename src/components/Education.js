64e782990e44d0a989c80167e9dbf8ad4d441106
Alphabarrie98 <barriealpha24@gmail.com>
2022-11-14 16:19:23 +0000

    initial commit

---
+++ src/components/Education.js
@@ -0,0 +1,25 @@
+import React from "react"
+default function Education{
+    return(
+        <div id="education" class="bg-[#
+            3EAEAEA] h-50 my-6 py-8 rounded-lg border-4">
+
+            <div class="flex justify-center">
+                <h3 class="border-4 w-20  border-dashed px-4 py-4">Education</h3>
+              </div>
+
+              
+              <details class="px-6 py-4">
+                <summary class="font-bold text-2xl text-gray-800">
+                  
+                  video game
+                  </summary>
+                <p>am attending bluecrest.</p>
+              </details>
+
+              
+            </div>
+            
+    )
+
+}
